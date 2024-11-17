b9da705f76fe31c4a0e4a1a28a00eec950b56e19
Aleksey Alekseev <a.alekseev@rdp.ru>
2024-11-17 19:15:56 +0300

    add reservation room logic and refactor endpoints

--- app/api/endpoints/meeting_room.py
+++ app/api/endpoints/meeting_room.py
@@ -1,18 +1,18 @@
-from http import HTTPStatus
-
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, Depends
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from app.api.validators import check_meeting_room_exists, check_name_duplicate
 from app.core.db import get_async_session
-from app.crud.meeting_room import create_meeting_room, get_room_id_by_name, \
-    read_all_rooms_from_db, get_meeting_room_by_id, update_meeting_room, \
-    delete_meeting_room
-from app.models.meeting_room import MeetingRoom
-from app.schemas.meeting_room import MeetingRoomCreate, MeetingRoomDB, \
-    MeetingRoomUpdate
+from app.crud.meeting_room import meeting_room_crud
+from app.crud.reservation import reservation_crud
 
-router = APIRouter(prefix='/meeting_room', tags=['meeting_room'])
+from app.schemas.meeting_room import (MeetingRoomCreate, MeetingRoomDB,
+                                      MeetingRoomUpdate)
+from app.schemas.reservation import ReservationDB
+
+router = APIRouter()
+
 
 @router.post(
     '/',
@@ -25,8 +25,9 @@ async def create_new_meeting_room(
         session: AsyncSession = Depends(get_async_session)
 ):
     await check_name_duplicate(meeting_room.name, session)
-    new_room = await create_meeting_room(meeting_room, session)
+    new_room = await meeting_room_crud.create(meeting_room, session)
     return new_room
+
 
 @router.get(
     '/',
@@ -36,8 +37,9 @@ async def create_new_meeting_room(
 async def get_all_meeting_rooms(
         session: AsyncSession = Depends(get_async_session)
 ):
-    all_rooms = await read_all_rooms_from_db(session)
+    all_rooms = await meeting_room_crud.get_multi(session)
     return all_rooms
+
 
 @router.patch(
     '/{meeting_room_id}',
@@ -57,35 +59,11 @@ async def partially_update_meeting_room(
     if obj_in.name is not None:
         await check_name_duplicate(obj_in.name, session)
 
-    meeting_room = await update_meeting_room(
+    meeting_room = await meeting_room_crud.update(
         meeting_room, obj_in, session
     )
     return meeting_room
 
-async def check_name_duplicate(
-        room_name: str,
-        session: AsyncSession
-) -> None:
-    room_id = await get_room_id_by_name(room_name, session)
-    if room_id is not None:
-        raise HTTPException(
-            status_code=HTTPStatus.UNPROCESSABLE_ENTITY,
-            detail='Переговорка с таким именем уже существует!'
-        )
-
-async def check_meeting_room_exists(
-        meeting_room_id: int,
-        session: AsyncSession
-) -> MeetingRoom:
-    meeting_room = await get_meeting_room_by_id(
-        meeting_room_id, session
-    )
-    if meeting_room is None:
-        raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND,
-            detail='Переговорка не найдена!'
-        )
-    return meeting_room
 
 @router.delete(
     '/{meeting_room_id}',
@@ -99,7 +77,23 @@ async def remove_meeting_room(
     meeting_room = await check_meeting_room_exists(
         meeting_room_id, session
     )
-    meeting_room = await delete_meeting_room(
+    meeting_room = await meeting_room_crud.remove(
         meeting_room, session
     )
     return meeting_room
+
+
+@router.get(
+    '/{meeting_room_id}/reservations',
+    response_model=list[ReservationDB]
+)
+async def get_reservations_for_room(
+        meeting_room_id: int,
+        session: AsyncSession = Depends(get_async_session),
+):
+    await check_meeting_room_exists(meeting_room_id, session)
+    reservations = await reservation_crud.get_future_reservations_for_room(
+        room_id=meeting_room_id,
+        session=session
+    )
+    return reservations
--- app/models/meeting_room.py
+++ app/models/meeting_room.py
@@ -1,4 +1,5 @@
 from sqlalchemy import Column, String, Text
+from sqlalchemy.orm import relationship
 
 from app.core.db import Base, PreBase
 
@@ -6,3 +7,4 @@ from app.core.db import Base, PreBase
 class MeetingRoom(Base, PreBase):
     name = Column(String(100), unique=True, nullable=False)
     description = Column(Text)
+    reservations = relationship('Reservation', cascade='delete')
