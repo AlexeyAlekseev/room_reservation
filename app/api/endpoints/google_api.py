5f4de90539bb146783d9f8b1286faf51a8ff272e
Aleksey Alekseev <a.alekseev@rdp.ru>
2024-12-11 12:54:09 +0300

    fix google cert issues

--- app/api/endpoints/google_api.py
+++ app/api/endpoints/google_api.py
@@ -9,29 +9,32 @@ from app.core.google_client import get_service
 from app.core.user import current_superuser
 
 from app.crud.reservation import reservation_crud
-from app.services.google_api import spreadsheets_create, set_user_permissions, \
-    spreadsheets_update_vlue
+from app.services.google_api import (
+    spreadsheets_create, set_user_permissions, spreadsheets_update_value)
 
 router = APIRouter()
 
+
 @router.post(
     '/',
-    response_model=list[dict[str, str]],
+    response_model=list[dict[str, int]],
     dependencies=[Depends(current_superuser)],
 )
 async def get_report(
         from_reserve: datetime,
         to_reserve: datetime,
         session: AsyncSession = Depends(get_async_session),
-        wrapper_service: Aiogoogle = Depends(get_service),
+        wrapper_services: Aiogoogle = Depends(get_service)
+
 ):
     """Только для суперюзеров."""
     reservations = await reservation_crud.get_count_res_at_the_same_time(
         from_reserve, to_reserve, session
     )
-    spreadsheetsid = await spreadsheets_create(wrapper_service)
-    await set_user_permissions(spreadsheetsid, wrapper_service)
-    await spreadsheets_update_vlue(
-        spreadsheetsid, reservations, wrapper_service
-    )
+    # Вызов функций
+    spreadsheetid = await spreadsheets_create(wrapper_services)
+    await set_user_permissions(spreadsheetid, wrapper_services)
+    await spreadsheets_update_value(spreadsheetid,
+                                    reservations,
+                                    wrapper_services)
     return reservations
--- app/services/google_api.py
+++ app/services/google_api.py
@@ -6,77 +6,72 @@ from app.core.config import settings
 
 FORMAT = "%Y/%m/%d %H:%M:%S"
 
-async def spreadsheets_create(wrapper_service: Aiogoogle) -> str:
+
+async def spreadsheets_create(wrapper_services: Aiogoogle) -> str:
+    # Получаем текущую дату для заголовка документа
     now_date_time = datetime.now().strftime(FORMAT)
-    service = await wrapper_service.discover("sheets", "v4")
+    # Создаём экземпляр класса Resource
+    service = await wrapper_services.discover('sheets', 'v4')
+    # Формируем тело запроса
     spreadsheet_body = {
-        'properties': {
-            'title': f'Отчет на {now_date_time}',
-            'locale': 'ru_RU'
-        },
-        'sheets': [
-            {
-                'properties': {
-                    'sheetType': 'GRID',
-                    'sheetId': 0,
-                    'title': 'Лист1',
-                    'gridProperties': {
-                        'rowCount': 100,
-                        'columnCount': 11
-                    }
-                }
-            }
-        ]
+        'properties': {'title': f'Отчёт на {now_date_time}',
+                       'locale': 'ru_RU'},
+        'sheets': [{'properties': {'sheetType': 'GRID',
+                                   'sheetId': 0,
+                                   'title': 'Лист1',
+                                   'gridProperties': {'rowCount': 100,
+                                                      'columnCount': 11}}}]
     }
-    response = await wrapper_service.as_service_account(
-        service.spreadsheets.create(
-            json=spreadsheet_body
-        )
+    # Выполняем запрос
+    response = await wrapper_services.as_service_account(
+        service.spreadsheets.create(json=spreadsheet_body)
     )
     spreadsheetid = response['spreadsheetId']
+    print(f'https://docs.google.com/spreadsheets/d/{spreadsheetid}')
     return spreadsheetid
+
 
 async def set_user_permissions(
         spreadsheetid: str,
-        wrapper_service: Aiogoogle,
+        wrapper_services: Aiogoogle
 ) -> None:
-    permissions_body = {
-        'type': 'user',
-        'role': 'writer',
-        'emailAddress': settings.email
-    }
-    service = await wrapper_service.discover("drive", "v3")
-    await wrapper_service.as_service_account(
+    permissions_body = {'type': 'user',
+                        'role': 'writer',
+                        'emailAddress': settings.email}
+    service = await wrapper_services.discover('drive', 'v3')
+    await wrapper_services.as_service_account(
         service.permissions.create(
             fileId=spreadsheetid,
             json=permissions_body,
-            fields='id'
-        )
-    )
+            fields="id"
+        ))
 
-async def spreadsheets_update_vlue(
+
+async def spreadsheets_update_value(
         spreadsheetid: str,
         reservations: list,
-        wrapper_service: Aiogoogle
+        wrapper_services: Aiogoogle
 ) -> None:
     now_date_time = datetime.now().strftime(FORMAT)
-    service = await wrapper_service.discover("sheets", "v4")
+    service = await wrapper_services.discover('sheets', 'v4')
+    # Здесь формируется тело таблицы
     table_values = [
         ['Отчёт от', now_date_time],
         ['Количество регистраций переговорок'],
         ['ID переговорки', 'Кол-во бронирований']
     ]
+    # Здесь в таблицу добавляются строчки
     for res in reservations:
-        now_res = [str(res['meetingroom_id']), str(res['count'])]
-        table_values.append(now_res)
+        new_row = [str(res['meetingroom_id']), str(res['count'])]
+        table_values.append(new_row)
 
     update_body = {
         'majorDimension': 'ROWS',
         'values': table_values
     }
-    response = await wrapper_service.as_service_account(
+    response = await wrapper_services.as_service_account(
         service.spreadsheets.values.update(
-            spreadsheetid=spreadsheetid,
+            spreadsheetId=spreadsheetid,
             range='A1:E30',
             valueInputOption='USER_ENTERED',
             json=update_body
